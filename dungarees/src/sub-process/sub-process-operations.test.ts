32e476e31a3bcd2d84cffccce20b63ffb737aa90
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2026-03-03 13:40:38 +0800

    👖 Format sub-process-operations

--- dungarees/src/sub-process/sub-process-operations.test.ts
+++ dungarees/src/sub-process/sub-process-operations.test.ts
@@ -1,11 +1,12 @@
-import { createFakeSpawn } from "./fake.ts"
-import { createSubProcessService } from "./service.ts"
-import { createSubProcessOperations } from "./sub-process-operations.ts"
-import { mtest } from "@dungarees/core/marbles-vitest.ts"
-import { stdout, stderr } from "@dungarees/cli/utils.ts"
+import { createFakeSpawn } from './fake.ts'
+import { createSubProcessService } from './service.ts'
+import { createSubProcessOperations } from './sub-process-operations.ts'
+
+import { stderr, stdout } from '@dungarees/cli/utils.ts'
+import { mtest } from '@dungarees/core/marbles-vitest.ts'
 import { createFakeFileSystem } from '@dungarees/fs/fake.ts'
-import { createProcessService } from "@dungarees/process/service.ts"
-import { createFakeNodeProcess } from "@dungarees/process/fake.ts"
+import { createFakeNodeProcess } from '@dungarees/process/fake.ts'
+import { createProcessService } from '@dungarees/process/service.ts'
 
 mtest('subProcessOperations.runSilentUntilError no output if no error', ({ expect }) => {
   const { spawn: fakeSpawn } = createFakeSpawn([
@@ -43,27 +44,37 @@ mtest('subProcessOperations.runSilentUntilError both output on error', ({ expect
   const subProcessService = createSubProcessService(fakeSpawn)
   const fakeNodeProcess = createFakeNodeProcess()
   const processService = createProcessService(fakeNodeProcess)
-  const subProcessOperations = createSubProcessOperations({ processService, subProcessService, fileSystem })
+  const subProcessOperations = createSubProcessOperations({
+    processService,
+    subProcessService,
+    fileSystem,
+  })
   const output = subProcessOperations.runSilentUntilError('ls', [])
   expect(output).toBeObservableStepAndClose([stdout('file.ts'), stderr('Error')])
 })
 
-mtest('subProcessOperations.isExecutable is true if the file other permissions is executable', ({ expect }) => {
-  const EXECUTABLE_PERMISSIONS = 0o001
-  const fileSystem = createFakeFileSystem({
-    '/exec-world.sh': '#!/bin/sh\necho hello',
-  })
-  fileSystem.chmodSync('/exec-world.sh', EXECUTABLE_PERMISSIONS)
-  const { spawn } = createFakeSpawn([])
-  const subProcessService = createSubProcessService(spawn)
-  const fakeNodeProcess = createFakeNodeProcess()
-  const processService = createProcessService(fakeNodeProcess)
-  const subProcessOperations = createSubProcessOperations({ processService, subProcessService, fileSystem })
-  const output = subProcessOperations.isExecutable('/exec-world.sh')
-  expect(output).toBeObservableStepAndClose(true)
-})
+mtest(
+  'subProcessOperations.isExecutable is true if the file other permissions is executable',
+  ({ expect }) => {
+    const EXECUTABLE_PERMISSIONS = 0o001
+    const fileSystem = createFakeFileSystem({
+      '/exec-world.sh': '#!/bin/sh\necho hello',
+    })
+    fileSystem.chmodSync('/exec-world.sh', EXECUTABLE_PERMISSIONS)
+    const { spawn } = createFakeSpawn([])
+    const subProcessService = createSubProcessService(spawn)
+    const fakeNodeProcess = createFakeNodeProcess()
+    const processService = createProcessService(fakeNodeProcess)
+    const subProcessOperations = createSubProcessOperations({
+      processService,
+      subProcessService,
+      fileSystem,
+    })
+    const output = subProcessOperations.isExecutable('/exec-world.sh')
+    expect(output).toBeObservableStepAndClose(true)
+  },
+)
 
-/*
 mtest('subProcessOperations.isExecutable is false if directory', ({ expect }) => {
   const EXECUTABLE_PERMISSIONS = 0o111
   const fileSystem = createFakeFileSystem()
@@ -73,30 +84,34 @@ mtest('subProcessOperations.isExecutable is false if directory', ({ expect }) =>
   const subProcessService = createSubProcessService(spawn)
   const fakeNodeProcess = createFakeNodeProcess()
   const processService = createProcessService(fakeNodeProcess)
-  const subProcessOperations = createSubProcessOperations({ processService, subProcessService, fileSystem })
+  const subProcessOperations = createSubProcessOperations({
+    processService,
+    subProcessService,
+    fileSystem,
+  })
   const output = subProcessOperations.isExecutable('/exec-world')
   expect(output).toBeObservableStepAndClose(false)
 })
 
-mtest.each([
-  0o000,
-  0o100,
-  0o010,
-  0o110,
-])('subProcessOperations.isExecutable is false if there are no executable permissions for the user', ({ expect }, permissions) => {
-  const NON_EXECUTABLE_PERMISSIONS = permissions
-  const fileSystem = createFakeFileSystem({
-    '/exec-world.sh': '#!/bin/sh\necho hello',
-  })
-  fileSystem.chmodSync('/exec-world.sh', NON_EXECUTABLE_PERMISSIONS)
+mtest.each([0o000, 0o100, 0o010, 0o110])(
+  'subProcessOperations.isExecutable is false if there are no executable permissions for the user',
+  ({ expect }, permissions) => {
+    const NON_EXECUTABLE_PERMISSIONS = permissions
+    const fileSystem = createFakeFileSystem({
+      '/exec-world.sh': '#!/bin/sh\necho hello',
+    })
+    fileSystem.chmodSync('/exec-world.sh', NON_EXECUTABLE_PERMISSIONS)
 
-  const { spawn } = createFakeSpawn([])
-  const subProcessService = createSubProcessService(spawn)
-  const fakeNodeProcess = createFakeNodeProcess({ userId: 1000 })
-  const processService = createProcessService(fakeNodeProcess)
-  const subProcessOperations = createSubProcessOperations({ processService, subProcessService, fileSystem })
-  const output = subProcessOperations.isExecutable('/exec-world.sh')
-  expect(output).toBeObservableStepAndClose(false)
-})
-
-*/
+    const { spawn } = createFakeSpawn([])
+    const subProcessService = createSubProcessService(spawn)
+    const fakeNodeProcess = createFakeNodeProcess({ userId: 1000 })
+    const processService = createProcessService(fakeNodeProcess)
+    const subProcessOperations = createSubProcessOperations({
+      processService,
+      subProcessService,
+      fileSystem,
+    })
+    const output = subProcessOperations.isExecutable('/exec-world.sh')
+    expect(output).toBeObservableStepAndClose(false)
+  },
+)
