0c3c6ac77e2492d580247df344c6683acb8a3e44
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2026-09-03 19:07:59 +0100

    🌏 Add pm-technical-fluency-validation-00 video

--- little-parrot/content/video/src/videos/index.ts
+++ little-parrot/content/video/src/videos/index.ts
@@ -1,7 +1,8 @@
+import pmTechnicalFluencyValidation00 from './pm-technical-fluency-validation-00'
 import social000 from './social-000'
 import social016 from './social-016'
 import social017 from './social-017'
 import social018 from './social-018'
 
 /** Every video in the project. The narrate script and the Remotion root both read this list. */
-export const VIDEOS = [social000, social016, social017, social018]
+export const VIDEOS = [social000, social016, social017, social018, pmTechnicalFluencyValidation00]
